1504641224265643aefb66e619c16476eeb6f48a
Joshua Norris <joshua.norris52@gmail.com>
2024-09-26 20:22:17 -0500

    Adding question for number 4 without solution.

---
+++ Problem Set 2/Question 4.hs
@@ -0,0 +1,19 @@
+
+-- A
+-- Write a functio that given the coefficients of a quadriatic a, b, and c will return how many roots the equation has
+
+numberNDroots :: Float -> Float -> Float -> Integer
+
+-- B
+-- Using your answer to the last question write a function that given the coefficients of the quadratic, a, b and c
+-- will return haw many roots the equation has. In the cas that the equation has every number a root you should return the result 3.
+
+numberRoots :: Float -> Float -> Float -> Integer
+
+-- C
+-- Write definitions of the functions which return the smaller and larger real roots of the quadratic. 
+-- In the case that the equation has no real roots or has all values as roots you should 
+-- return zero as the result of each of the functions.
+
+smallerRoot :: Float-> Float-> Float-> Float
+largerRoot :: Float-> Float-> Float-> Float
