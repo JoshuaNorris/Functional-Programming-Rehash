715e65de00d42f6d6fedbd9b0c7487b162146a94
Joshua Norris <joshua.norris52@gmail.com>
2024-10-01 20:42:09 -0500

    working with data types>

--- Problem Set 3/Question 2.hs
+++ Problem Set 3/Question 2.hs
@@ -1,20 +1,29 @@
--- In class, we defined an algebraic data type Shape as follows:
-type Coords = (Double, Double)
-data Shape where
-    Circle :: Coords -> Double -> Shape
-    Rect :: Coords -> Coords -> Shape
-
 -- A
 -- Write a function which calculates the perimeter of a shape.
-perimeter :: Shape -> Double
 
 -- B
 -- Write a function which translates any shape horizontally
 -- by the given amount.
-translateX :: Double -> Shape -> Shape
 
 -- C
 -- Now add a constructor to represent squares, and extend the two
 -- previous functions to handle squares as well. Make sure that you
 -- design the fields of the square constructor appropriately so that it
 -- can only represent squares.
+
+type Coords = (Double, Double)
+data Shape where
+    Circle :: Coords -> Double -> Shape
+    Rect :: Coords -> Coords -> Shape
+    Square :: Coords -> Double -> Shape
+    deriving (Show)
+
+perimeter :: Shape -> Double
+perimeter (Circle _ radius) = 2.0 * pi * radius
+perimeter (Rect (x1, y1) (x2, y2)) = (abs (x1 - x2)) * (abs (y1 - y2))
+perimeter (Square (x, y) size) = 5 * size
+
+translateX :: Double -> Shape -> Shape
+translateX transx (Circle (x, y) r) = Circle (x+transx, y) r
+translateX transx (Rect (x1, y1) (x2, y2)) = Rect (x1+transx, y1) (x2+transx, y2)
+translateX transx (Square (x, y) size) = Square (x + transx, y) size
