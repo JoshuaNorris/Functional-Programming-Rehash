52ca5159594c233c71c737eb5d54507816a09775
Joshua Norris <joshua.norris52@gmail.com>
2024-10-02 20:28:10 -0500

    Question 5

--- Problem Set 3/Question 5.hs
+++ Problem Set 3/Question 5.hs
@@ -1,34 +1,11 @@
--- Exercise 4 The standard Haskell library defines a data type
--- data Maybe a where
--- Nothing :: Maybe a
--- Just :: a -> Maybe a
+-- Exercise 5 The standard Haskell library also defines a data type
+-- data Either a b where
+-- Left :: a -> Either a b
+-- Right :: b -> Either a b
 
--- It is typically used to model potential failure: a value of type Maybe a
--- might contain a value of type a, or it might be Nothing. Implement
--- each of the following functions:
+-- Implement a function of type
+-- (a, Either b c) -> Either (a,b) (a,c)
 
--- (a) add :: Maybe Int -> Maybe Int -> Maybe Int, which tries to
--- add two numbers, but returns Nothing if either of its arguments
--- is.
-
-add :: Maybe Int -> Maybe Int -> Maybe Int
-add Nothing _ = Nothing
-add _ Nothing = Nothing
-add (Just x) (Just y) = Just (x+y)
-
--- (b) divide :: Integer -> Integer -> Maybe Integer, which tries
--- to compute the integer quotient of its two arguments, but fails if
--- the second argument is zero.
-
-divide :: Integer -> Integer -> Maybe Integer
-divide x 0 = Nothing
-divide x y = Just (div x y)
-
--- (c) collapse :: Maybe (Maybe a) -> Maybe a, which returns a
--- value of type a (wrapped in Just) if at all possible, or Nothing
--- otherwise.
-
-collapse :: Maybe (Maybe a) -> Maybe a
-collapse Nothing = Nothing
-collapse (Just Nothing) = Nothing
-collapse (Just (Just x)) = Just x
+splitEither :: a -> Either b c -> Either (a,b) (a,c)
+splitEither a (Left b) = Left (a,b)
+splitEither a (Right c) = Right (a,c)
